fc1dffc117d199d9eccee842a8b8a53c31aa6146
Piyush <piyushnegi310@gmail.com>
2026-07-04 16:50:10 +0530

    add GET products route with hardcoded data

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import List
 app=FastAPI()
 
-@app.get("/") #decorator used to define end point
+class Product(BaseModel):
+    id:int
+    name:str
+    price:float
+
+product=[
+{"id": 1, "name": "Laptop", "price": 55000.0},
+    {"id": 2, "name": "Mouse", "price": 500.0},
+    {"id": 3, "name": "Keyboard", "price": 1200.0},
+
+]
+
+
+@app.get("/")
 def home():
-    return {"message":"hello"}
+    return {"message": "hello"}
+
+
+@app.get("/products",response_model=List[Product]) 
+def get_products():
+    return product
